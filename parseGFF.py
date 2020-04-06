0f4a3835852aca70d4952bf89455d242339cf27e
Andrew Alverson <aja@Andrews-MBP.attlocal.net>
2020-04-06 08:51:30 -0500

    parse the GFF with csv module

--- parseGFF.py
+++ parseGFF.py
@@ -1,40 +1,51 @@
 #! /usr/bin/env python3
 
+import csv
+
 # specify the input files
 gff_file   = 'watermelon.gff'
 fasta_file = 'watermelon.fsa'
-
-
-# open the GFF file
-gff = open(gff_file, 'r')
 
 
 # list for gene names
 gene_names = []
 
 # read GFF file, line by line
-for line in gff:
-
-	# remove line breaks
-	line = line.rstrip('\n')
+with open(gff_file, 'r') as gff:
 	
-	# split each line on the tab character
-	# sequence, source, feature, begin, end, length, strand, phase, attributes = line.split('\t')
-	fields = line.split('\t')
+	# create a csv reader object
+	reader = csv.reader(gff, delimiter="\t")
 
-	# split the attributes field (index = 8), each attribute is separated by ' ; '
+	for line in reader:
+		# skip blank lines
+		if not line:
+			continue
 
-	attributes = fields[8].split(' ; ')
+		else:
+			print(line[3], line[4])
 
-	# we only need the first item in this list
-	# print(attributes[0])
-	gene_fields = attributes[0].split('Gene ')
+# for line in gff:
 
-	# print the gene name
-	# print(gene_fields[1])
+# 	# remove line breaks
+# 	line = line.rstrip('\n')
+	
+# 	# split each line on the tab character
+# 	# sequence, source, feature, begin, end, length, strand, phase, attributes = line.split('\t')
+# 	fields = line.split('\t')
 
-	# store the gene name
-	gene_names.append(gene_fields[1])
+# 	# split the attributes field (index = 8), each attribute is separated by ' ; '
+
+# 	attributes = fields[8].split(' ; ')
+
+# 	# we only need the first item in this list
+# 	# print(attributes[0])
+# 	gene_fields = attributes[0].split('Gene ')
+
+# 	# print the gene name
+# 	# print(gene_fields[1])
+
+# 	# store the gene name
+# 	gene_names.append(gene_fields[1])
 
 
 	# extract the DNA sequence from the genome for this feature
@@ -44,8 +55,8 @@ for line in gff:
 	# calculate the GC content for this feature, and print it to the screen
 
 # print the genes in alphabetical order
-for gene in sorted(gene_names):
-	print(gene)
+# for gene in sorted(gene_names):
+# 	print(gene)
 
 
 
